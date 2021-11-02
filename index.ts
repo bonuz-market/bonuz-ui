7c30f44ce96f1e7f1f5e320facf17c9a65cafb22
SergeyBeltsin <beltsins89@gmail.com>
2021-11-02 22:20:04 +0300

    export all ui components from src

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from './src/shared/ui/atoms';
+export * from './src/shared/ui/molecules';
+export * from './src/entities/news';
+export * from './src/features/subscribe-to-news';
--- src/entities/news/index.ts
+++ src/entities/news/index.ts
@@ -1,4 +1,2 @@
-export { newsModel } from './model';
-export type { NewsItem } from './model';
 export { NewsCard } from './ui/news-card';
 export { NewsRow } from './ui/news-row';
