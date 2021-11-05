fa82817275efac58e2f51843a714be0a3fa5f4c2
SergeyBeltsin <beltsins89@gmail.com>
2021-11-05 20:00:22 +0300

    fix links in navigation

--- src/components/atoms/navigation/navigation.tsx
+++ src/components/atoms/navigation/navigation.tsx
@@ -3,28 +3,22 @@ import { FC } from 'react';
 export const Navigation: FC = () => (
   <>
     <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#">How it works</a>
+      <a href="#four">How it works</a>
     </li>
     <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#">Join the Waitlist</a>
+      <a href="#subscribe">Join the Waitlist</a>
     </li>
     <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#">Token</a>
+      <a href="#eight">Token</a>
     </li>
     <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#">Roadmap</a>
+      <a href="#six">Roadmap</a>
     </li>
     <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#">Team</a>
+      <a href="#nine">Team</a>
     </li>
     <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#">Contact Us</a>
+      <a href="#subscribe">Private Sale</a>
     </li>
   </>
 );
