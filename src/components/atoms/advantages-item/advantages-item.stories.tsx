f0cdb94f732c333d266fb85eb789f44d1c15cc21
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-11-25 00:48:06 +0200

    refactor: advantages-item component

--- src/components/atoms/advantages-item/advantages-item.stories.tsx
+++ src/components/atoms/advantages-item/advantages-item.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, Story } from '@storybook/react';
 import { AdvantagesItem, AdvantagesItemProps } from './advantages-item';
 import mockData from '../../../mock/mock-data.json';
+import './advantages-wrapper.scss';
 
 export default {
   title: 'Components/Atoms/AdvantagesItem',
