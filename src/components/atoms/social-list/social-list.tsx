46d392fee11752f3e67689def3f1e48d5e666d87
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-12-06 14:29:25 +0200

    refactor: apply stylelint changes

--- src/components/atoms/social-list/social-list.tsx
+++ src/components/atoms/social-list/social-list.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import './social-list.scss';
 
 export const SocialList: FC = () => (
-  <div className="icon_list">
+  <div className="icon-list">
     <a href="#">
       <img src="/assets/images/telegram.svg" alt="" />
     </a>
