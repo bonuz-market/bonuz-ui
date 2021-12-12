899f66d593cfc45e61003980e06fdd8e82b45ae2
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-12-12 19:03:35 +0200

    components: team-card links for people

--- src/components/atoms/team-card/team-card.tsx
+++ src/components/atoms/team-card/team-card.tsx
@@ -5,12 +5,15 @@ export type TeamCardProps = {
   img: string;
   name: string;
   position: string;
+  link: string;
 };
 
-export const TeamCard: FC<TeamCardProps> = ({ img, name, position }) => (
+export const TeamCard: FC<TeamCardProps> = ({ img, name, position, link }) => (
   <div className="item">
     <img src={img} className="pic" alt="" />
     <p>{name}</p>
-    <span>{position}</span>
+    <span>
+      <a href={link}>{position}</a>
+    </span>
   </div>
 );
