34bcf04c285220dc78288f0d922d27cd68150320
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-01-03 14:31:40 +0200

    components:statElement: remove unused scss

--- src/components/atoms/stat-element/stat-element.stories.tsx
+++ src/components/atoms/stat-element/stat-element.stories.tsx
@@ -1,24 +1,13 @@
 import { Meta, Story } from '@storybook/react';
 import { StatElement, StatElementProps } from './stat-element';
 import mockData from '../../../mock/mock-data.json';
-import './stat-wrapper.scss';
 
 export default {
   title: 'Components/Atoms/StatElement',
   component: StatElement,
 } as Meta;
 
-const Template: Story<StatElementProps> = (args) => (
-  <div id="eight">
-    <div className="row">
-      <div className="col">
-        <div className="blocks">
-          <StatElement {...args} />
-        </div>
-      </div>
-    </div>
-  </div>
-);
+const Template: Story<StatElementProps> = (args) => <StatElement {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = mockData.statistic.map((item) => ({
