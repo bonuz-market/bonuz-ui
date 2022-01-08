03254127e247f79f689a4643511a02d22b8774c8
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-01-08 22:54:19 +0200

    components:advantages: changed to swiper slider

--- src/components/molecules/advantages-list/advantages-list.tsx
+++ src/components/molecules/advantages-list/advantages-list.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/no-array-index-key */
 import { FC } from 'react';
 
-import Slider, { Settings } from 'react-slick';
+import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination } from 'swiper';
 import { AdvantagesItem } from '../../atoms';
 import mockData from '../../../mock/mock-data.json';
 import '../../atoms/advantages-item/advantages-wrapper.scss';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export type AdvantagesListProps = {
   items: {
@@ -14,42 +18,37 @@ export type AdvantagesListProps = {
   }[];
 };
 
-const settings: Settings = {
+const swiperOptions: SwiperProps = {
   className: 'blocks',
-  dots: true,
-  arrows: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  rtl: mockData.rtlLanguages.includes(localStorage.getItem('i18nextLng') || ''),
-  customPaging(index: number): JSX.Element {
-    return <button type="button" aria-label={`Go to ${index + 1} slide`} />;
+  loop: true,
+  dir: mockData.rtlLanguages.includes(localStorage.getItem('i18nextLng') || '')
+    ? 'rtl'
+    : 'ltr',
+  pagination: { clickable: true },
+
+  breakpoints: {
+    1050: {
+      slidesPerView: 1,
+    },
+    1051: {
+      slidesPerView: 3,
+      allowTouchMove: false,
+    },
   },
-  responsive: [
-    {
-      breakpoint: 4000,
-      settings: 'unslick',
-    },
-    {
-      breakpoint: 1050,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
 };
+SwiperCore.use([Pagination]);
 
 export const AdvantagesList: FC<AdvantagesListProps> = ({ items }) => (
-  <Slider {...settings}>
+  <Swiper {...swiperOptions}>
     {items.map((item, index) => (
-      <AdvantagesItem
-        key={`${item.title}-${index}`}
-        title={item.title}
-        description={item.description}
-        icon={item.icon}
-      />
+      <SwiperSlide>
+        <AdvantagesItem
+          key={`${item.title}-${index}`}
+          title={item.title}
+          description={item.description}
+          icon={item.icon}
+        />
+      </SwiperSlide>
     ))}
-  </Slider>
+  </Swiper>
 );
