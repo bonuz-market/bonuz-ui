f3fa7e5d2df36ebe3350bd56a99144f22736848e
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-01-19 01:39:25 +0200

    components:how-it-works: fix wrong import

--- src/components/molecules/how-it-works-list/how-it-works-list.tsx
+++ src/components/molecules/how-it-works-list/how-it-works-list.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Pagination } from 'swiper';
 import { HowItWorksItem } from '../../atoms';
-import './how-it-works-wrapper.scss';
+import '../../atoms/how-it-works-item/how-it-works-wrapper.scss';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
