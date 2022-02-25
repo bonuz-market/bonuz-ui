5382e7ecf11348abd76a12c2aa916d16d34a1237
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-02-25 20:18:06 +0200

    components:Header: added container

--- src/components/molecules/Header/HeaderSecondary.tsx
+++ src/components/molecules/Header/HeaderSecondary.tsx
@@ -14,23 +14,27 @@ export const HeaderSecondary: FC<HeaderSecondaryProps> = ({
   description,
 }) => (
   <header className="header">
-    <div className="header__navbar">
-      {navText && (
-        <a href="/" className="header__back">
-          {navText}
-        </a>
-      )}
-      {logo && (
-        <div className="header__logo">
-          <a href="/" className="header__logo-link">
-            <img src={logo} alt="Logo" />
+    <div className="container">
+      <div className="header__navbar">
+        {navText && (
+          <a href="/" className="header__back">
+            {navText}
           </a>
-        </div>
-      )}
-    </div>
-    <div className="header__info">
-      <h1 className="header__info-title">{title}</h1>
-      {description && <p className="header__info-description">{description}</p>}
+        )}
+        {logo && (
+          <div className="header__logo">
+            <a href="/" className="header__logo-link">
+              <img src={logo} alt="Logo" />
+            </a>
+          </div>
+        )}
+      </div>
+      <div className="header__info">
+        <h1 className="header__info-title">{title}</h1>
+        {description && (
+          <p className="header__info-description">{description}</p>
+        )}
+      </div>
     </div>
   </header>
 );
