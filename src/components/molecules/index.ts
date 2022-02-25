8ac03301829be45bcde696eb711c551097607f64
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-02-25 11:21:25 +0200

    components: fix missing exports

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,12 +1,12 @@
-export { Header } from './Header';
-export { Footer } from './Footer';
+export { Header, HeaderSecondary } from './Header';
+export { Footer, FooterSecondary } from './Footer';
 export { CelebritiesList } from './CelebritiesList';
 export { HowItWorksList } from './HowItWorksList';
 export { RoadMapList } from './RoadMapList';
 export { PartnersList } from './PartnersList';
 export { AdvantagesList } from './AdvantagesList';
 export { TeamList } from './TeamList';
-export { Tabs } from './Tabs';
+export { Tabs, TabsSecondary } from './Tabs';
 export { NewsRow } from './NewsRow';
 export { Media } from './Media';
 export { Users } from './Users';
