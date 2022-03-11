c117c1965eb773fd14512fdbb798a7459534959e
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-03-11 14:51:23 +0200

    components: fix duplicate case sensitive folders

--- src/components/molecules/header/index.ts
+++
@@ -1,2 +0,0 @@
-export { Header } from './Header';
-export { HeaderSecondary } from './HeaderSecondary';
--- src/components/molecules/tabs/index.ts
+++
@@ -1,2 +0,0 @@
-export { Tabs } from './Tabs';
-export { TabsSecondary } from './TabsSecondary';
