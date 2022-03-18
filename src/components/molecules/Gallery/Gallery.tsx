ccd73df59c9a18379f151491c0339025c1fa9419
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-03-18 22:36:22 +0200

    components:Gallery: added lazy loading

--- src/components/molecules/Gallery/Gallery.tsx
+++ src/components/molecules/Gallery/Gallery.tsx
@@ -5,18 +5,27 @@ import './Gallery.scss';
 
 export type GalleryProps = {
   items: { image: string; link?: string; alt?: string }[];
+  lazy?: boolean;
 };
 
-export const Gallery: FC<GalleryProps> = ({ items }) => (
+export const Gallery: FC<GalleryProps> = ({ items, lazy }) => (
   <div className="gallery">
     {items.map((item, index) => (
       <div className="item" key={index}>
         {item.link ? (
           <a href={item.link} target="_blank" rel="noreferrer">
-            <img src={item.image} alt={item.alt || `item ${index}`} />
+            <img
+              src={item.image}
+              alt={item.alt || `item ${index}`}
+              loading={lazy ? 'lazy' : undefined}
+            />
           </a>
         ) : (
-          <img src={item.image} alt={item.alt || `item ${index}`} />
+          <img
+            src={item.image}
+            alt={item.alt || `item ${index}`}
+            loading={lazy ? 'lazy' : undefined}
+          />
         )}
       </div>
     ))}
