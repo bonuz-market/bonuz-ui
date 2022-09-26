2c57bfe675291055126fe8bc61ede8a6ecb32d31
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-09-26 19:32:20 +0200

    feat(components): navigation: add dropdown chevron

--- src/components/atoms/Navigation/Navigation.tsx
+++ src/components/atoms/Navigation/Navigation.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import classNames from 'classnames';
 import React, { FC, useState } from 'react';
+import { DropdownChevron } from '../Icons';
 import './Navigation.scss';
 
 interface NavigationProps {
@@ -23,7 +24,7 @@ export const Navigation: FC<React.PropsWithChildren<NavigationProps>> = ({
           onMouseEnter={() => setIsOpen(true)}
           onMouseLeave={() => setIsOpen(false)}
         >
-          {title}
+          {title} <DropdownChevron width={16} height={16} className="chevron" />
           {isOpen && (
             <ul
               className={classNames('nav-submenu', {
